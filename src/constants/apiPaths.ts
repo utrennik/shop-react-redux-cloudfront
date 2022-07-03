459131487d67464248a5b0439c359276d66eea4e
Utrennik <utrennik@list.ru>
2022-07-03 14:05:03 +0300

    feat: implement task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://6db2h6g43k.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://jdr8r8f7m1.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
